22876596fa2dfe0f5b5177cdb7724eb3bb84731c
Pol Santamaria <polsm91@gmail.com>
2020-01-24 16:27:50 +0100

    Script to initialize hands-on

---
+++ init_hecuba_handson.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /opt
+
+# Activate virtualenv for hecuba
+source /opt/venv-hecuba/bin/activate
+
+# Install Hecuba
+pip3 install hecuba
+
+# Clone hands-on
+git clone https://github.com/polsm91/Hecuba-Hands-on.git /opt/hecuba_handson
+
+cd /opt/hecuba_handson
+
+# Launch Jupyter notebook
+jupyter notebook --ip=0.0.0.0 --NotebookApp.token=''
